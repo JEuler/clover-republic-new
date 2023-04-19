e58eb61b7cf272703d2611c52c375b8158987d02
Ivan Terekhin <231950+JEuler@users.noreply.github.com>
2023-04-19 13:11:37 +0600

    Icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
+import logo from "../assets/images/flag.png";
+
 const navbarLinks = [
   { label: "Home", href: "index.html", ariaLabel: "Home" },
-  { label: "Privacy policy", href: "privacy_policy.html", ariaLabel: "Privacy policy" },
+  {
+    label: "Privacy policy",
+    href: "privacy_policy.html",
+    ariaLabel: "Privacy policy",
+  },
 ];
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full h-20 flex flex-col justify-center items-center fixed bg-customDarkBg1 lg:bg-customDarkBgTransparent z-40 lg:backdrop-blur-xl">
-      <div className="2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative">
+    <nav className='w-full h-20 flex flex-col justify-center items-center fixed bg-customDarkBg1 lg:bg-customDarkBgTransparent z-40 lg:backdrop-blur-xl'>
+      <div className='2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative'>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
           exit={{ opacity: 0 }}
         >
-          <a className="navbar-link" href="#home" aria-label="Home">
-            <div className="flex justify-start items-center grow basis-0">
-              <div className="text-white mr-2 text-6xl">
-
+          <a className='navbar-link' href='#home' aria-label='Home'>
+            <div className='flex justify-start items-center grow basis-0'>
+              <div className='text-white mr-2 text-6xl'>
+                <img width="24" height="24" src={logo} />
               </div>
               <div className="text-white font-['Inter'] font-bold text-xl">
                 Clover Republic
@@ -35,10 +41,10 @@ export const Navbar = () => {
           transition={{ duration: 0.3 }}
           exit={{ opacity: 0 }}
         >
-          <div className="hidden lg:flex h-full pl-12 pb-2">
+          <div className='hidden lg:flex h-full pl-12 pb-2'>
             {navbarLinks.map(({ href, label, ariaLabel }) => (
               <a
-                className="navbar-link"
+                className='navbar-link'
                 href={href}
                 aria-label={ariaLabel}
                 key={label}
@@ -49,12 +55,12 @@ export const Navbar = () => {
           </div>
         </motion.div>
         <div
-          className="lg:hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2"
+          className='lg:hidden flex flex-col  px-2 py-3 border-solid border border-gray-600 rounded-md cursor-pointer hover:bg-customDarkBg2'
           onClick={() => setIsOpen(!isOpen)}
         >
-          <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
-          <div className="w-5 h-0.5 bg-gray-500  mb-1"></div>
-          <div className="w-5 h-0.5 bg-gray-500 "></div>
+          <div className='w-5 h-0.5 bg-gray-500  mb-1'></div>
+          <div className='w-5 h-0.5 bg-gray-500  mb-1'></div>
+          <div className='w-5 h-0.5 bg-gray-500 '></div>
         </div>
       </div>
       {/* Mobile navbar */}
@@ -67,14 +73,14 @@ export const Navbar = () => {
             exit={{ opacity: 0 }}
           >
             <div
-              className="flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full
+              className='flex flex-col mt-16 lg:hidden absolute top-4 left-0  bg-customDarkBg1 z-50 w-full
         items-center gap-10 pb-10 border-y border-solid border-customDarkBg3 pt-10
-        "
+        '
             >
               {navbarLinks.map(({ label, href, ariaLabel }) => (
                 <a
                   key={href}
-                  className="navbar-link"
+                  className='navbar-link'
                   href={href}
                   onClick={() => setIsOpen(false)}
                   aria-label={ariaLabel}
